88b3f59a91398ae729ebe2575700cb6f73a0e7e6
Thomas Bundy <57776191+bundy92@users.noreply.github.com>
2023-02-05 14:23:20 +0100

    Initial commit.

---
+++ Stock_research.py
@@ -0,0 +1,10 @@
+#1 install and import baseline deopendecies
+
+#2 setup summarization model
+
+#3 summarize a single article
+
+#4 building a news and sentiment pipeline
+
+#4.1 strip out unwanted URLs
+
